d9bdaa37b6d556898cd8b08a2c25135a201e73c5
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-09-29 00:54:25 -0700

    Install devise gem and begin routing to devise pages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import "@rails/ujs" // This is enough to start UJS functionality
 import "controllers"
+
+// Ensure Rails UJS starts
+import Rails from "@rails/ujs";
+Rails.start();
