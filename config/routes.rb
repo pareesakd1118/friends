5aed69fe4192ded06433d881a9dd264a148bec6a
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-09-28 18:06:58 -0700

    Update FE so that friends page is in table form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :friends
-  # get "home/index"
+
   root 'home#index' 
   get "home/about"
- get 'friends', to: 'friends#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
